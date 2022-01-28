2f8ebc29d88962302087f327f6b00103c6c870cb
Charles Callender <charlescallender.cc@gmail.com>
2022-01-28 06:21:47 -0800

    Updated gitignore

--- api/verifyToken.js
+++ api/verifyToken.js
@@ -6,7 +6,7 @@ function verify(req,res,next) {
     if(authHeader) {
         const token = authHeader.split(" ")[1];
 
-        jwt.verify(token, process.env.SECRET_KEY, (err,user) => {
+        jwt.verify(token, process.env.SECRET_KEY, (err,user) => {a
             if(err) res.status(403).json("Token not valid");
             req.user = user;
             next();
